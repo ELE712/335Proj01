77e3ef817e583d836ee34d27fd74181a25f2177d
ELE712 <zandber2@msu.edu>
2016-01-30 16:15:13 -0500

    Added code
    Still need to test w/ completed CEmployee/CManager

--- CDatabase.h
+++ CDatabase.h
@@ -1,20 +1,54 @@
 /******************************************************
-** FILE:
+** FILE: CDatabase.h
 **
-** ABSTRACT:
+** ABSTRACT: Holds database class for employees
 **
 **
 **
-** AUTHOR:
+** AUTHOR: Ezekiel Zandbergen, 
 **
-** CREATION DATE:
+** CREATION DATE: 01-27-16
 **
 *******************************************************/
 
 #ifndef CDATABASE_H
 #define CDATABASE_H
 
+#include <vector>
+#include <string>
+#include "CEmployee.h"
+#include "CManager.h"
+
+using std::string;
+
+
 class CDatabase{
+private:
+    CDatabase(){} //default constructor not needed
+    
+protected:
+    vector<CEmployee*> m_allEmp; //to hold all employees
+    
+public:
+    CDatabase(vector<CEmployee*> emp_vec){
+        //constructs vector from referenced vector
+        for (int i=0; i<allEmp.size(); ++i){
+            AddRecord(emp_vec[i]);
+        }
+    }
+    
+    virtual ~CDatabase(){
+        m_allEmp.clear(); //clears vector holding all employees
+    }
+    
+    void AddRecord(const CEmployee emp){
+        m_allEmp.push_back(&emp);
+    }
+    
+    void DisplayRecords(){
+        for (int i=0; i<m_allEmp.size(); ++i)
+            allEmp[i]->DisplayEmployee(); //might be allEmp[i].DisplayEmployee()
+    }
     
 };
 
